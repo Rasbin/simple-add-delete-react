f42414cd74ef9b7d0addb226dd83edd552c4f10b
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-09-23 14:33:38 +0300

    Simple add and delete data in React finalized - playing around with react :)

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
+
+class App extends Component {
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
+
+    state = {
+        characters: []
+    };
+
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <Table
+                    characterData={this.state.characters}
+                    removeCharacter={this.removeCharacter}
+                />
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
+}
+
+export default App;
